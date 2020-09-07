3d57b772db361823ea9dba44492fe99fe061c4dd
radupasparuga <radupasparuga25@gmail.com>
2020-09-07 21:36:16 +0300

    Small style change

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -158,7 +158,6 @@ export default defineComponent({
   width: 50%;
   height: 50%;
   margin: auto;
-  margin-top: 20px;
   padding: 20px;
   position: relative;
   box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0);
